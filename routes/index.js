2fec4299ad3688a2e756c0f1b393146263de1e5f
Michael Valenta <d22623@oaopava.cz>
2024-12-08 19:36:57 +0100

    Website and Error html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Serve the main index.html file
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'html', 'index.html'));
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Example user route
+router.get('/', (req, res) => {
+  res.send('User route');
 });
 
 module.exports = router;
