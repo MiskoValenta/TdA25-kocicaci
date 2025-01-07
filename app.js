7af4140c3466748d007c7ae7cf7d68cefe73ba1f
Michael Valenta <d22623@oaopava.cz>
2025-01-07 10:13:04 +0100

    app.js
    Adding app.js for overview, /login and /user

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'views' folder
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'views', 'overview', 'index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'overview', 'index.html'));
+  app.use(express.static(path.join(__dirname, 'views', 'overview')));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login', 'login.html'));
+  app.use(express.static(path.join(__dirname, 'views', 'login')));
+});
+
+app.get('/user', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'user.html'));
+});
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).sendFile(path.join(__dirname, 'views', 'error', 'error.html'));
+  app.use(express.static(path.join(__dirname, 'views', 'error')));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
